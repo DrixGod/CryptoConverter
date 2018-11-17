186d4fd12ded93d326d992dc665f70cb6fe77395
isvor <isvoranandrei96@gmail.com>
2018-11-17 17:04:18 +0200

    Added get location functionality

---
+++ app/src/main/java/company/cryptoconvertor/CoinList.java
@@ -0,0 +1,14 @@
+package company.cryptoconvertor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CoinList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.coin_layout);
+    }
+
+}
--- app/src/main/java/company/cryptoconvertor/MainActivity.java
+++ app/src/main/java/company/cryptoconvertor/MainActivity.java
@@ -1,71 +1,106 @@
 package company.cryptoconvertor;
 
-import android.Manifest;
 import android.annotation.SuppressLint;
-import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
 import android.widget.TextView;
-
+import android.widget.Toast;
+import java.io.IOException;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button b;
-    private TextView t;
-    private TextView countryTextView;
-    private LocationManager locationManager;
     double LAT;
     double LNG;
-
+    TextView locationTextView;
+    TextView details;
+    private String countryName;
+    private String countryCode;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
-        t = (TextView) findViewById(R.id.textView);
-        b = (Button) findViewById(R.id.button);
-        countryTextView = findViewById(R.id.text10);
+        locationTextView = findViewById(R.id.location);
+        details = findViewById(R.id.details);
 
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        LocationManager locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
 
-        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED){
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
             return;
         }
 
-        Location location = locationManager.getLastKnownLocation(locationManager.NETWORK_PROVIDER);
-
+        assert locationManager != null;
+        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
         onLocationChanged(location);
-        loc_func(location);
+        getLocationDetails();
+
+        //Change activiy -> pentru debug comenteaza asta
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, CoinList.class));
+            }
+        };
+        Handler h = new Handler();
+        h.postDelayed(r, 2500);
+
     }
 
     @SuppressLint("SetTextI18n")
     public void onLocationChanged(Location location){
-        LAT = location.getLongitude();
-        LNG = location.getLatitude();
+        LAT = location.getLatitude();
+        LNG = location.getLongitude();
+        locationTextView.setText("Latitude: " + LAT + "\nLongitude: " + LNG);
     }
 
-    private void loc_func(Location location){
-        try{
+    @SuppressLint("SetTextI18n")
+    private void getLocationDetails(){
+        try {
             Geocoder geocoder = new Geocoder(this);
-            List<Address> addressList = null;
+            List<Address> addressList;
             addressList = geocoder.getFromLocation(LAT,LNG,1);
-            String country = addressList.get(0).getCountryName();
-            countryTextView.setText(country);
-        }catch (Exception ignored){
+            String countryName = addressList.get(0).getCountryName();
+            String countryCode = addressList.get(0).getCountryCode();
+            setCountryName(countryName);
+            setCountryCode(countryCode);
+            details.setText("Country: " + countryName + "\n" + "Code: " + countryCode);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "Error: " + e, Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
     }
 }
